38f285127b2784b3e7d5fefeb79f1a616bc29776
MOLEDesign <Development@moledesign.biz>
2015-05-05 08:10:24 +0100

    final feed

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-$nationalData=file_get_contents("national.json");
-$fgwData=file_get_contents("fgw.json");
+$shareprice=file_get_contents("shareprice.json");
 
-$national=json_decode($nationalData);
-$fgw=json_decode($fgwData);
+$sharepriceData=json_decode($shareprice);
 
 $date = date("D, j M Y G:i:s O");
 
@@ -15,40 +13,26 @@ echo "<rss version=\"2.0\">\n";
 echo "<channel>\n";
 echo "<lastBuildDate>$date</lastBuildDate>\n";
 echo "<pubDate>$date</pubDate>\n";
-echo "<title>National PPM</title>\n";
-echo "<description><![CDATA[[National UK PPM Summary]]]></description>\n";
-echo "<link>http://apps.gwtrains.co.uk/uksummary</link>\n";
+echo "<title>FGW Shareprice</title>\n";
+echo "<description><![CDATA[[FGW Shareprice Summary]]]></description>\n";
+echo "<link>http://www.firstgroup.com</link>\n";
 echo "<language>English</language>\n";
 echo "<managingEditor>morgan.leecy@firstgroup.com</managingEditor>\n";
 echo "<webMaster>morgan.leecy@firstgroup.com</webMaster>\n";
 
 
-$currentPPM = $national->PPM->text;
-$rollingPPM = $national->RollingPPM->text;
-$totalTrains = $national->Total;
-$totalOnTime = $national->OnTime;
-$totalLate = $national->Late;
-$totalCancel = $national->CancelVeryLate;
+$currentPrice = $sharepriceData->Bid;
+$dailyChange = $sharepriceData->PercentChange;
+$dailyLow = $sharepriceData->DaysLow;
+$dailyHigh = $sharepriceData->DaysHigh;
+$yearLow = $sharepriceData->YearLow;
+$yearHigh = $sharepriceData->YearHigh;
 
-$description = "Total Trains : $totalTrains<br />Trains : $totalOnTime<br />Late : $totalLate<br/>Cancel / Very Late : $totalCancel";
+$description = "Daily Low : $dailyLow<br />Daily High : $dailyHigh<br />Years Low : $yearLow<br />Years High : $yearHigh";
 
 echo "<item>\n";
-echo "<title>National PPM - Current $currentPPM% (last 2 hours $rollingPPM%)</title>\n";
+echo "<title>Current Price : $currentPrice (change $dailyChange)</title>\n";
 echo "<description>".htmlspecialchars($description)."</description>\n";
-echo "</item>\n";
-
-$FGWPPM = $fgw->PPM->text;
-$FGWrollingPPM = $fgw->RollingPPM->text;
-$FGWtotalTrains = $fgw->Total;
-$FGWtotalOnTime = $fgw->OnTime;
-$FGWtotalLate = $fgw->Late;
-$FGWtotalCancel = $fgw->CancelVeryLate;
-
-$FGWdescription = "Total Trains : $FGWtotalTrains<br />Trains : $FGWtotalOnTime<br />Late : $FGWtotalLate<br/>Cancel / Very Late : $FGWtotalCancel";
-
-echo "<item>\n";
-echo "<title>FGW PPM - Current $FGWPPM% (last 2 hours $FGWrollingPPM%)</title>\n";
-echo "<description>".htmlspecialchars($FGWdescription)."</description>\n";
 echo "</item>\n";
 
 echo "</channel>";
